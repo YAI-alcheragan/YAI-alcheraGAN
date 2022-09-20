9b4cf163493510eed758bd43a0f51e7c787c8d12
suuuuuuuubin <52194394+suuuuuuuubin@users.noreply.github.com>
2022-09-20 21:59:42 +0900

    add metrics

---
+++ metric_dataset.py
@@ -0,0 +1,37 @@
+import torch
+import torch.utils.data
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+import os
+
+import numpy as np
+from PIL import Image
+
+    
+
+class MetricDataset(Dataset):
+    def __init__(self, data_root, load_size=256) :
+        
+        self.data_root = data_root
+        
+        self.img_ls = [os.path.join(data_root, img) for img in os.listdir(self.data_root)]
+        
+        self.transform = transforms.Compose([transforms.Resize([load_size, load_size]),
+                                              transforms.ToTensor(), 
+                                             ]) # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        
+    def __getitem__(self, idx):
+        
+        img_path = self.img_ls[idx]
+        img = Image.open(img_path).convert("RGB")
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        
+        return img
+    
+        
+    def __len__(self):
+        
+        return len(self.img_ls)
