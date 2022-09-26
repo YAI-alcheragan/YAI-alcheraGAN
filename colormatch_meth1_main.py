84bc70a7c015707e41f7dfaeee12acc9e266a7e3
“이름” <reccos1020@gmail.com>
2022-09-26 23:43:49 +0900

    organize code

--- colormatch_meth1_main.py
+++ colormatch_meth1_main.py
@@ -8,10 +8,13 @@ from collections import Counter
 from skimage.color import rgb2lab, deltaE_cie76
 import matplotlib.pyplot as plt
 
-def RGB2HEX(color):
-    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
 
 def get_colors(img, number_of_colors):
+    """
+    Extract top colors
+    img: image of which colors we wish to extract
+    number_of_colors: total # of colors we want to extract
+    """
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.reshape(img.shape[0]*img.shape[1], 3)
 
@@ -23,14 +26,14 @@ def get_colors(img, number_of_colors):
 
     # We get ordered colors by iterating through the keys
     ordered_colors = [center_colors[i] for i in counts.keys()]
-    # hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
     rgb_colors = ordered_colors
-    # rgb_colors = [ordered_colors[i] for i in counts.keys()]
-
+    
     return rgb_colors
 
 def match_image_by_color(image, color, threshold = 60, number_of_colors = 10): 
-    
+    """
+    filter all images that match the selected color
+    """
     image_colors = get_colors(image, number_of_colors)
     selected_color = rgb2lab(np.uint8(np.asarray([[color]])))
 
@@ -45,6 +48,9 @@ def match_image_by_color(image, color, threshold = 60, number_of_colors = 10):
 
 
 def selectiveSearch(bg_img, cropped_img, num, search_cnt=500):
+    """
+    find location to paste through selective search
+    """
     h, w, _ = bg_img.shape
     ch, cw, _ = cropped_img.shape
     i = 0
@@ -52,24 +58,19 @@ def selectiveSearch(bg_img, cropped_img, num, search_cnt=500):
     COLORS = {'GREEN': [0, 128, 0]}
     while i < search_cnt and not found:
         point = (random.randint(0, w - cw), random.randint(0, h - ch))
-        # crop_location_img = bg_img[point[1]:point[1]+ch, point[0]:point[0]+cw]
         below_crop_location_img = bg_img[point[1]+ch-int(ch*0.2):point[1]+ch, point[0]:point[0]+cw]
         selected = match_image_by_color(below_crop_location_img, COLORS['GREEN'], 70, 3)
-        #print(below_crop_location_img.shape)
 
-        #print(i+1,"th image trial")
         if selected:
-        # # 수정 전 (-)    
-        #     bg_img[point[1]:point[1]+ch, point[0]:point[0]+cw] = cropped_img
-        #     copy_paste_img = bg_img
-        #     cv2.imwrite("copy_paste_img{}.jpg".format(num),copy_paste_img)
             found = True
         i+=1
         
-# 수정 후 (+)    
     if not i < search_cnt :
         print("selectiveSearch failed")
     return point
+
+
+
 
 if __name__ == "__main__":
     number_of_imgs = 10
