136c99f91cca4e41da30b358afdb551ecbc48886
lsh-159 <110099021+lsh-159@users.noreply.github.com>
2022-10-11 19:46:34 +0900

    return to old version for run train.py
    return to blend_dataset.py in gpgan branch old version, 
    training uses this blend_dataset.py
    inference uses inference_blend_dataset.py

--- blend_dataset.py
+++ blend_dataset.py
@@ -14,31 +14,47 @@ from colormatch_meth1_main import selectiveSearch
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class BlendingDataset(Dataset):
-    def __init__(self, root, bg_folder, obj_folder, mode, load_size=0):
-      # Assuming objimg.jpg, objimg.json in obj_folder
-        self.root = root
-        self.bg_folder = os.path.join(root, mode, bg_folder)
-        self.obj_folder = os.path.join(root, mode, obj_folder)
+  def __init__(self, root, bg_folder, obj_folder, obj_ann, mode, load_size):
+    
+    self.root = root
+    self.bg_folder = os.path.join(root, mode, bg_folder)
+    self.obj_folder = os.path.join(root, mode, obj_folder)
+    self.mode = mode
+    self.load_size = load_size
 
-        print(f"DEBUG // Assuming bg_folder has many [no_fire.jpg]  obj_folder has many [smoke.jpg, label.json, mask.jpg]")
-        print(f"DEBUG // Assuming bg_folder data structure[{self.bg_folder}]     ==   data structure[datasets/skipped]  ")
-        print(f"DEBUG // Assuming obj_folder data structure[{self.obj_folder}]  ==  data structure[datasets/confirmed] ")
-        print(f"DEBUG // Make sure you have folders -> bg_folder, obj_folder")
-        print(f"\t-> current bg_folder = {self.bg_folder}\n\t-> current obj_folder = {self.obj_folder}")
-        
-        self.mode = mode
-        self.load_size = load_size
+    self.bg_imgs = os.listdir(self.bg_folder)
+    obj_ann_df =  pd.read_csv(os.path.join(self.obj_folder, obj_ann)) #dataset 구조: root/train/smoke/img1.jpg // root/train/smoke/_annotations.csv
+    self.obj_anns = obj_ann_df.values.tolist()
+    self.obj_anns_len = len(self.obj_anns)
+    
+    self.transform = self.get_transform()
+    
+  def __getitem__(self, idx):
+    bg = cv2.imread(os.path.join(self.bg_folder,self.bg_imgs[idx]))
+    bg_h, bg_w, _ = bg.shape
+    random_idx = np.random.randint(len(self.obj_anns))
+    # roboflow (Tensorflow Object Detection CSV format)
+    obj_path, _, _, _, x1, y1, x2, y2 = self.obj_anns[random_idx] #get random obj img info
+    obj = cv2.imread(os.path.join(self.obj_folder,obj_path))
+    obj_h, obj_w, _ = obj.shape
 
-        self.bg_imgs = sorted(glob.glob(self.bg_folder+"/*/images/cur/*.jpg"))
-        self.obj_imgs = sorted(glob.glob(self.obj_folder+"/*/images/cur/*.jpg"))
-        self.bg_imgs = self.obj_imgs
-        self.label_paths = sorted(glob.glob(self.obj_folder+"/*/labels/*.json"))
-        self.mask_img_paths = sorted(glob.glob(self.obj_folder+"/*/masks/*.jpg"))
-        
-        self.transform = self.get_transform()
+    ratio = min(bg_w/obj_w, bg_h/obj_h)   #bg와 obj의 크기 차이를 고려해 crop한 이미지의 사이즈를 조정
+    if bg_w <= int(x2-x1) or bg_h<=int(y2-y1) :
+      obj_crop = cv2.resize(obj[y1:y2,x1:x2], (int((x2-x1)*ratio), int((y2-y1)*ratio))) # *ratio
+    else :
+      # print("no ratio")
+      obj_crop = cv2.resize(obj[y1:y2,x1:x2], (int((x2-x1)), int((y2-y1)))) # 
+    crop_h, crop_w, _ = obj_crop.shape
 
-        print(f"DEBUG // bg_imgs # : {len(self.bg_imgs)}  obj_imgs # : {len(self.obj_imgs)}")
-        print("blendingDataset __init__  finished")
+
+    bg_crop_x, bg_crop_y = np.random.randint(bg_w - crop_w), np.random.randint(bg_h - crop_h)
+
+    copy_paste = bg.copy()
+    copy_paste[bg_crop_y:bg_crop_y+crop_h, bg_crop_x:bg_crop_x+crop_w] = obj_crop
+
+    copy_paste, bg = self.transform(copy_paste), self.transform(bg)
+
+    return copy_paste, bg 
 
     def show_tensor(self, tensor, save=False, index=0):
         x = tensor.permute((1, 2, 0))
@@ -46,72 +62,6 @@ class BlendingDataset(Dataset):
         x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
         cv2.imshow("tensor", x)
         cv2.waitKey()
-
-    def __getitem__(self, idx):
-        bg = np.array(Image.open(self.bg_imgs[idx]))
-        bg_h, bg_w, _ = bg.shape
-
-        random_idx = np.random.randint(len(self.obj_imgs))
-
-        obj_path = self.obj_imgs[random_idx]
-        
-        obj_label_path= self.label_paths[random_idx]
-        obj_mask_path = self.mask_img_paths[random_idx]
-
-        # bbox 좌표 로드
-        with open(obj_label_path, "r") as js_file:
-            obj_label = json.load(js_file)
-        points = obj_label["objects"][0][0]['points']
-        x1, y1, x2, y2 = points[0]['x'], points[0]['y'], points[1]['x'], points[1]['y']
-        obj = np.array(Image.open(obj_path))
-        mask = np.array(Image.open(obj_mask_path))
-
-        obj_h, obj_w, _ = obj.shape
-
-        print(f"DEBUG // bg.shape ={bg.shape}, obj.shape={obj.shape}, mask.shape={mask.shape}")
-      
-        ratio = min(bg_w/obj_w, bg_h/obj_h)   
-        if bg_w <= int(x2-x1) or bg_h<=int(y2-y1) :
-            cropped_obj = Image.fromarray(obj[y1:y2,x1:x2])
-            obj_crop = np.array(cropped_obj.resize((int((x2-x1)*ratio), int((y2-y1)*ratio)))) # *ratio
-            cropped_mask = Image.fromarray(mask[y1:y2,x1:x2])
-            mask_crop = np.array(cropped_mask.resize((int((x2-x1)*ratio), int((y2-y1)*ratio)))) 
-        else :
-            cropped_obj = Image.fromarray(obj[y1:y2,x1:x2])
-            obj_crop = np.array(cropped_obj.resize((int((x2-x1)), int((y2-y1)))))#
-            cropped_mask = Image.fromarray(mask[y1:y2,x1:x2])
-            mask_crop = np.array(cropped_mask.resize((int((x2-x1)), int((y2-y1)))))#
-        crop_h, crop_w, _ = obj_crop.shape
-
-        debug_cropped_obj = np.zeros_like(obj)
-        debug_cropped_obj[y1:y2,x1:x2] = np.array(cropped_obj)
-        debug_cropped_mask = np.zeros_like(mask)
-        debug_cropped_mask[y1:y2,x1:x2] = np.array(cropped_mask)
-
-
-        # color matching 으로 bg 이미지 위에 croped_obj(smoke)를 copy_paste할 적절한 위치 좌표 받음 
-        bg_crop_x, bg_crop_y = selectiveSearch(bg, obj_crop, 0, search_cnt=1) 
-
-        # 기존에 있던 연기 마스크를 새로운 좌표로 옮겨줘야 함 
-        mask_new = np.zeros_like(bg)
-        mask_new[bg_crop_y:bg_crop_y+crop_h, bg_crop_x:bg_crop_x+crop_w, :] = mask_crop
-        # mask[bg_crop_y:bg_crop_y+crop_h, bg_crop_x:bg_crop_x+crop_w, :] = 255     
-
-        # 기존에 있던 연기 사진도 새로운 좌표로 옮겨줘야 하는데, 이 경우 그냥 bg 이미지에 먼저 copy_paste 해버리는걸로
-        copy_paste = bg.copy()
-        copy_paste[bg_crop_y:bg_crop_y+crop_h, bg_crop_x:bg_crop_x+crop_w] = obj_crop
-        
-        # copy_paste 미리 해도 문제가 없는 이유 : gpgan의 input은 이미 copy_paste된 이미지이기 때문에
-        copy_paste_init = copy_paste * mask_new + bg*(1-mask_new)
-
-        obj, copy_paste, bg, mask_new = self.transform(obj), self.transform(copy_paste_init), \
-                                    self.transform(bg), self.transform(mask_new)
-
-
-        return {"obj": obj, "bg" : bg, "cp": copy_paste , "mask": mask_new, \
-                "mask_old" : self.transform(mask), "cropped_obj": self.transform(debug_cropped_obj), "cropped_mask" : self.transform(debug_cropped_mask),\
-                "cp_old" : self.transform(copy_paste) } 
-
 
 
     def __len__(self):
